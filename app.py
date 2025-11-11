009ccf9c51a1f6daffda1831b086833822e9f900
pxc-naosato <noboru.naosato@pixel-cz.co.jp>
2025-11-11 14:21:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -64,15 +64,15 @@ def parse_date_like(v) -> Union[date, None]:
         except Exception:
             return None
 
-    if isinstance(v, (int, float)):
-        try:                
-            # Excelのシリアル値（1900/1/1ベース）として変換を試みる
-            # '1899-12-30' はExcelの1900年閏年バグを考慮した起点
-            temp_date = pd.to_datetime(v, unit='D', origin='1899-12-30')
-            
-            return temp_date.date()
-        except Exception:
-            pass # シリアル値でなかった場合は、下の文字列処理へ
+    #if isinstance(v, (int, float)):
+    #    try:                
+     #       # Excelのシリアル値（1900/1/1ベース）として変換を試みる
+      #      # '1899-12-30' はExcelの1900年閏年バグを考慮した起点
+       #     temp_date = pd.to_datetime(v, unit='D', origin='1899-12-30')
+        #    
+         #   return temp_date.date()
+       # except Exception:
+        #    pass # シリアル値でなかった場合は、下の文字列処理へ
     
     s = safe_str(v)
     if not s:
