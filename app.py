33da9fb6ef9f3ca689e9206da6e6e62c3347c203
pxc-naosato <noboru.naosato@pixel-cz.co.jp>
2025-11-13 16:33:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -743,18 +743,25 @@ if st.button("スキルシートを生成 (Excel形式)"):
             cur += 1
         cur += 1 # 空白行
 
-        # --- 14行目: 3. 開発経験サマリ ---
-        cell = ws.cell(row=cur, column=1, value="3. 開発経験サマリ")
+        # --- 11行目: 開発経験サマリ ---
+        cell = ws.cell(row=cur, column=2, value="開発経験サマリ")
         style(cell, font=section_title_font)
         cur += 1
         
         # サマリ本文もテーブル幅(K列)まで結合
-        ws.merge_cells(start_row=cur, start_column=1, end_row=cur, end_column=TABLE_COLS)
+        ws.merge_cells(start_row=cur, start_column=2, end_row=cur, end_column=TABLE_COLS)
         style(ws.cell(row=cur, column=1, value=st.session_state.pi_summary), align=wrap_text_alignment)
         cur += 2 # 空白行を1つ挟む
 
+        # --- 13行目: 作業工程・役割 ---
+        cur += 1
+        ws.cell(row=cur, column=2, value="業務内容：１．調査分析、要件定義  ２：基本（外部）設計　３．詳細（内部）設計　４．ｺｰﾃﾞｨﾝｸﾞ・単体ﾃｽﾄ　5．ＩＴ・ＳＴ")
+        ws.cell(row=cur, column=2, value="6.システム運用・保守　7．サーバー構築・運用管理　8．DB構築・運用管理　9．ネットワーク運用保守　10．ヘルプ・サポート 11．その他")
+        ws.cell(row=cur, column=2, value="参加形態：（PM)プロジェクトマネージャー　（PL)プロジェクトリーダー　（SPL）サブリーダー　（SE）システムエンジニア （PG）プログラマー")
+        cur += 1
+        
         # --- 17行目: 4. 業務経歴 ---
-        cell = ws.cell(row=cur, column=1, value="4. 業務経歴")
+        cell = ws.cell(row=cur, column=2, value="業務経歴")
         style(cell, font=section_title_font)
         cur += 1
 
@@ -846,6 +853,10 @@ if st.button("スキルシートを生成 (Excel形式)"):
         ws.column_dimensions["J"].width = 8.6 # ポジション
         ws.column_dimensions["K"].width = 11 # 規模
 
+        ws.row_dimensions[1].height = 43
+        ws.row_dimensions[2].height = 30
+        ws.row_dimensions[3].height = 30
+    
     st.download_button(
         label="スキルシートをダウンロード",
         data=output.getvalue(),
