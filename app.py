bb31f59f89a95e7de78bcde5a0bf819e85befd1f
pxc-naosato <noboru.naosato@pixel-cz.co.jp>
2025-11-14 15:42:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -849,7 +849,7 @@ if st.button("スキルシートを生成 (Excel形式)"):
             st.write(os)
             
             for model in range(len(os)):
-                ws.cell(row=start_row, column=6, value=os[model])
+                ws.cell(row=start_row, column=6, value=os[0])
             
             # --- 8行目 (言語/ツール・DB/DC) ---
 
