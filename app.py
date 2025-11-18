c914ae5ccfd37a50371f278c14efaf93b15d7a94
pxc-naosato <noboru.naosato@pixel-cz.co.jp>
2025-11-18 18:13:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -929,7 +929,7 @@ if st.button("スキルシートを生成 (Excel形式)"):
                 style(ws.cell(row=start_row + j, column=TABLE_COLS), border=Border(right=Side(style='thick')))
 
             for j in range(8):
-                if j = 0:
+                if j == 0:
                     style(ws.cell(row=start_row, column=3 + j), border=Border(top=Side(style='thick')))
                     style(ws.cell(row=end_row, column=3 + j), border=Border(bottom=Side(style='thick')))
                 else:
