59ae286f4fb3db7614a1575eb2d6319e335ce8d3
pxc-naosato <noboru.naosato@pixel-cz.co.jp>
2025-11-19 18:00:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -342,8 +342,8 @@ def parse_projects(df: pd.DataFrame) -> list:
                 #        proc_labels.append(WORK_PROCESS_MAP[k])
         else:
             for key, label_name in WORK_PROCESS_MAP.items():
-                st.write(key)
                 if label_name == str(s):
+                    st.write(label_name)
                     proc_labels.append(label_name)
     
 
