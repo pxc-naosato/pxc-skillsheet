5be78335b41ebf3193cfb5eba5a2fdb318a1ec08
pxc-naosato <noboru.naosato@pixel-cz.co.jp>
2025-11-19 18:09:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -295,7 +295,7 @@ def parse_projects(df: pd.DataFrame) -> list:
         proc_labels = []
         for s in cur["procs"]:
             s_raw = s.strip()
-            
+            st.write(s_raw)
             if looks_like_proc_codes(s_raw):
                 s_normalized = s_raw.translate(str.maketrans({
                     # 全角数字 -> 半角数字
@@ -342,7 +342,7 @@ def parse_projects(df: pd.DataFrame) -> list:
                 #        proc_labels.append(WORK_PROCESS_MAP[k])
         else:
             for key, label_name in WORK_PROCESS_MAP.items():
-                st.write(label_name, ":", s_raw, " ", s, label_name == s_raw)
+                #st.write(label_name, ":", s_raw, " ", s, label_name == s_raw)
                 if label_name == s_raw:
                     proc_labels.append(label_name)
     
