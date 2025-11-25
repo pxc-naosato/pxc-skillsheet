a652500daaf5dbfd00dce778b25f9786c5565282
pxc-naosato <noboru.naosato@pixel-cz.co.jp>
2025-11-25 14:13:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -493,10 +493,10 @@ def load_from_excel_callback():
     gdrive_url = st.session_state.gdrive_url
     
     if gdrive_url is None:
+        st.write(gdrive_url.split('/d/')[1].split('/')[0])
         file_id = gdrive_url.split('/d/')[1].split('/')[0]
         download_url = f"https://drive.google.com/uc?export=download&id={file_id}"
         content = requests.get(download_url).content
-        st.write(download_url, content)
     try:
         xl = pd.ExcelFile(uploaded_file)
         df = choose_best_sheet(xl)
