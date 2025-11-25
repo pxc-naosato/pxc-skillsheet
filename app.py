a5ec5e3a9f475caa4cacaca4b817a5dc67ed6d26
pxc-naosato <noboru.naosato@pixel-cz.co.jp>
2025-11-25 15:26:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -488,25 +488,35 @@ initialize_session_state()
 # =========================
 # コールバック
 # =========================
-def load_from_excel_callback():
+def load_from_excel_callback(drive: bool):
     gdrive_url = st.session_state.gdrive_url
-    
-    if gdrive_url is None:
-        st.write("読み込めていません")
-        return
+
+    if drive == True:
+        if gdrive_url is None:
+            st.write("読み込めていません")
+            return
+        else:
+            file_id = gdrive_url.split('/d/')[1].split('/')[0]
+            download_url = f"https://drive.google.com/uc?export=download&id={file_id}"
+            content = requests.get(download_url).content
     else:
-        file_id = gdrive_url.split('/d/')[1].split('/')[0]
-        download_url = f"https://drive.google.com/uc?export=download&id={file_id}"
-        content = requests.get(download_url).content
+        if uploaded_file is None:
+        return
 
     try:
-        xl = io.BytesIO(content)
-        df = pd.read_excel(xl)
-        st.write(df)
+        if drive == True:
+            xl = io.BytesIO(content)
+            df = pd.read_excel(xl)
         
-        if df is None:
-            st.error("有効なシートが見つかりませんでした。")
-            return
+            if df is None:
+                st.error("有効なシートが見つかりませんでした。")
+                return
+        else:
+            xl = pd.ExcelFile(uploaded_file)
+            df = choose_best_sheet(xl)
+            if df is None:
+                st.error("有効なシートが見つかりませんでした。")
+                return
 
         # --- 個人情報＆資格 ---
         pi = read_personal(df)
@@ -608,12 +618,12 @@ uploaded_file = st.file_uploader(
     "Excelファイル（.xlsx推奨）",
     type=["xlsx", "csv"],
     key="excel_uploader",
-    on_change=load_from_excel_callback)
+    on_change=load_from_excel_callback(False))
 
 
 url = st.text_input("Google Driveの共有リンクを入力",
     key="gdrive_url",
-    on_change=load_from_excel_callback)
+    on_change=load_from_excel_callback(True))
 
 def basic_info():
     st.header("個人情報")
