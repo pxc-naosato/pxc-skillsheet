fe4ecd898138217cb0194141e22bbae4d508efa7
pxc-naosato <noboru.naosato@pixel-cz.co.jp>
2025-11-26 10:19:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -489,7 +489,7 @@ initialize_session_state()
 # コールバック
 # =========================
 def load_from_excel_callback(drive: bool):
-    uploaded_file = st.session_state.excel_uploader
+    uploaded_file = st.session_state.get("excel_uploader")
     if uploaded_file is None:
         return
     try:
